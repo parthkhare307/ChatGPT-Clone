2fbae5055bdc2cc89b48ea0ee7541d7533f47928
Parth Khare <parthkhare307@gmail.com>
2023-04-25 16:37:06 +0530

    Making Side-Bar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 const App = () => {
   return (
     <div className="app">
+      <section className="side-bar">
+        <button>+ New Chat</button>
+        <ul className="history"></ul>
+        <nav>
+          <p>Made by Parth</p>
+        </nav>
+      </section>
+
+
+      <section className="main"></section>
     </div>
   )
 }
