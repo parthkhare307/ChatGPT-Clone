e32a5cdcc107a9d0715bb7b437052fe799c66b07
Parth Khare <parthkhare307@gmail.com>
2023-04-25 21:35:30 +0530

    Start working on Main Section

--- src/App.js
+++ src/App.js
@@ -3,16 +3,36 @@ const App = () => {
     <div className="app">
       <section className="side-bar">
         <button>+ New Chat</button>
-        <ul className="history"></ul>
+        <ul className="history">
+          <li>BLUGH</li>
+        </ul>
         <nav>
           <p>Made by Parth</p>
         </nav>
       </section>
 
 
-      <section className="main"></section>
+      <section className="main">
+        <h1>ParthGPT</h1>
+        <ul className="feed">
+
+        </ul>
+        <div className="bottom-section">
+          <div className="input-container">
+            <input/>
+            <div id="submit">➢</div>
+          </div>
+          <p className="info">
+            Chat GPT Mar 14 Version. Free Research Preview.
+            Our goal is to make AI systems more natural and safe to interact with.
+            Your feedback will help us improve.
+          </p>
+
+        </div>
+      </section>
     </div>
   )
 }
 
 export default App
+ 
